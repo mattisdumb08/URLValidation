945fdebd59cd01a5f95835714de07159f96aa060
Matt <91633363+mattisdumb08@users.noreply.github.com>
2026-07-26 01:19:12 +0100

    Added a console prompts no change in package.json; white space got removed README updated

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 It's supposed to check an entered URL against a list of common websites.
 If it seems as if the URL has been misspelt then it flags it up.
+
+Learning JS
--- source/main.js
+++ source/main.js
@@ -1,3 +1,31 @@
 const fs = require("fs")
+const readLine = require("readline")
 
-console.log("Hello World!")
+// https://stackoverflow.com/questions/61394928/get-user-input-through-node-js-console
+const rl = readLine.createInterface(
+    {
+        input : process.stdin,
+        output : process.stdout,
+    }
+);
+
+const prompt = function(question){
+    return new Promise(function(resolve){
+        rl.question(question , function(answer){
+            resolve(answer);
+        });
+    });
+}
+
+async function main(){
+    const input = await prompt("Enter some text: ");
+
+    console.log(input);
+
+    console.log("Hello World!");
+
+    rl.close()
+
+}
+
+main()
